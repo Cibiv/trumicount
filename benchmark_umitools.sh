45a0bbec69b2a9b8d5ad88e3abfe9f7a078b2e7d
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-09-27 19:14:25 +0200

    Repeat benchmark 3 times

--- benchmark_umitools.sh
+++ benchmark_umitools.sh
@@ -14,43 +14,45 @@ INPUTS="ec.1.bam|20|p|100|1 \
 exec > >(tee benchmark_umitools.log) 2>&1
 source ~/Installs/miniconda2/bin/activate
 
-for input in $INPUTS; do
-	input_split=(${input//|/ })
-	bam=${input_split[0]}
-	mapq=${input_split[1]}
-	case "${input_split[2]}" in
-		p)
-			paired_opts="--combine-strand-umis"
-		;;
-		u)
-			paired_opts=""
-		;;
-	esac
-	th=${input_split[3]}
-	mol=${input_split[4]}
+for rep in 1 2 3; do
+        for input in $INPUTS; do
+                input_split=(${input//|/ })
+                bam=${input_split[0]}
+                mapq=${input_split[1]}
+                case "${input_split[2]}" in
+                        p)
+                                paired_opts="--combine-strand-umis"
+                        ;;
+                        u)
+                                paired_opts=""
+                        ;;
+                esac
+                th=${input_split[3]}
+                mol=${input_split[4]}
 
-	echo "==== PYTHON 2.7 $bam PATCHED PAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
-	./trueumis.R \
-		--input-bam $bam --umi-sep='|' --paired --umipair-sep=: \
-		--mapping-quality $mapq $paired_opts --threshold $th --molecules $mol \
-		--verbose
+                echo "==== PYTHON 2.7 $bam PATCHED PAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
+                ./trueumis.R \
+                        --input-bam $bam --umi-sep='|' --paired --umipair-sep=: \
+                        --mapping-quality $mapq $paired_opts --threshold $th --molecules $mol \
+                        --verbose
 
-	echo "==== PYTHON 2.7 $bam PATCHED UNPAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
-	./trueumis.R \
-		--input-bam $bam --umi-sep='|' \
-		--mapping-quality $mapq --threshold $th --molecules $mol \
-		--verbose
+                echo "==== PYTHON 2.7 $bam PATCHED UNPAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
+                ./trueumis.R \
+                        --input-bam $bam --umi-sep='|' \
+                        --mapping-quality $mapq --threshold $th --molecules $mol \
+                        --verbose
 
-	echo "==== PYTHON 2.7 $bam UNPATCHED PAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
-	./trueumis.R \
-		--input-bam $bam --umi-sep='|' --paired \
-		--mapping-quality $mapq --threshold $th --molecules $mol \
-		--verbose
+                echo "==== PYTHON 2.7 $bam UNPATCHED PAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
+                ./trueumis.R \
+                        --input-bam $bam --umi-sep='|' --paired \
+                        --mapping-quality $mapq --threshold $th --molecules $mol \
+                        --verbose
 
-	echo "==== PYTHON 2.7 $bam UNPATCHED UNPAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
-	./trueumis.R \
-		--input-bam $bam --umi-sep='|' \
-		--mapping-quality $mapq --threshold $th --molecules $mol \
-		--verbose
+                echo "==== PYTHON 2.7 $bam UNPATCHED UNPAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
+                ./trueumis.R \
+                        --input-bam $bam --umi-sep='|' \
+                        --mapping-quality $mapq --threshold $th --molecules $mol \
+                        --verbose
 
+        done
 done
