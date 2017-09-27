8b68b07a62e4c2a62b5b0270b42e9c0a04e9d311
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-09-27 18:29:39 +0200

    Run benchmark for different input files

--- benchmark_umitools.sh
+++ benchmark_umitools.sh
@@ -4,43 +4,53 @@ set -e
 set -o pipefail
 
 export PYTHONNOUSERSITE=1
-export INBAM=ec.1.bam
-#export INBAM=ut.small.bam
+INPUTS="ec.1.bam|20|p|100|1 \
+        ec.2.bam|20|p|100|1 \
+        dm.1.bam|20|u|5|2 \
+        dm.2.bam|20|u|2|2"
+#INPUTS="ut.small.bam|20|p|100|1"
 
+# Setup python & logging
 exec > >(tee benchmark_umitools.log) 2>&1
-
 source ~/Installs/miniconda2/bin/activate
-echo "********************************************************************************"
-echo "==== PYTHON 2.7 EC.1 PATCHED PAIRED ============================================"
-echo "********************************************************************************"
-./trueumis.R \
-	--input-bam $INBAM --umi-sep='|' --paired --umipair-sep=: \
-	--mapping-quality 20 --combine-strand-umis --threshold 100 --molecules 2 \
-	--verbose
 
-source ~/Installs/miniconda2/bin/activate
-echo "********************************************************************************"
-echo "==== PYTHON 2.7 EC.1 PATCHED UNPAIRED =========================================="
-echo "********************************************************************************"
-./trueumis.R \
-	--input-bam $INBAM --umi-sep='|' \
-	--mapping-quality 20 --threshold 50 --molecules 1 \
-	--verbose
+for input in $INPUTS; do
+	input_split=(${input//|/ })
+	bam=${input_split[0]}
+	mapq=${input_split[1]}
+	case "${input_split[2]}" in
+		p)
+			paired_opts="--combine-strand-umis"
+		;;
+		u)
+			paired_opts=""
+		;;
+	esac
+	th=${input_split[3]}
+	mol=${input_split[4]}
 
-source ~/Installs/miniconda2/bin/activate py27_orig
-echo "********************************************************************************"
-echo "==== PYTHON 2.7 EC1. UNPATCHED PAIRED =========================================="
-echo "********************************************************************************"
-./trueumis.R \
-	--input-bam $INBAM --umi-sep='|' --paired \
-	--mapping-quality 20 --threshold 50 --molecules 1 \
-	--verbose
+	echo "==== PYTHON 2.7 $bam PATCHED PAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
+	./trueumis.R \
+		--input-bam $bam --umi-sep='|' --paired --umipair-sep=: \
+		--mapping-quality $mapq $paired_opts --threshold $th --molecules $mol \
+		--verbose
 
-source ~/Installs/miniconda2/bin/activate py27_orig
-echo "********************************************************************************"
-echo "==== PYTHON 2.7 EC.1 UNPATCHED UNPAIRED ========================================"
-echo "********************************************************************************"
-./trueumis.R \
-	--input-bam $INBAM --umi-sep='|' \
-	--mapping-quality 20 --threshold 50 --molecules 1 \
-	--verbose
+	echo "==== PYTHON 2.7 $bam PATCHED UNPAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
+	./trueumis.R \
+		--input-bam $bam --umi-sep='|' \
+		--mapping-quality $mapq --threshold $th --molecules $mol \
+		--verbose
+
+	echo "==== PYTHON 2.7 $bam UNPATCHED PAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
+	./trueumis.R \
+		--input-bam $bam --umi-sep='|' --paired \
+		--mapping-quality $mapq --threshold $th --molecules $mol \
+		--verbose
+
+	echo "==== PYTHON 2.7 $bam UNPATCHED UNPAIRED MAPQ=$mapq THRESHOLD=$th MOLECULES=$mol ==============="
+	./trueumis.R \
+		--input-bam $bam --umi-sep='|' \
+		--mapping-quality $mapq --threshold $th --molecules $mol \
+		--verbose
+
+done
