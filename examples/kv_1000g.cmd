4a6beffb3a9b8a6c37e5fea5c3470d40df232cf3
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-10-05 13:15:24 +0200

    Updated to use new gwpcR version which distinguishes between number of UMIs and observations.
    Re-created the example outputs

---
+++ examples/kv_1000g.cmd
@@ -0,0 +1,5 @@
+trumicount --input-bam kv_1000g.bam \
+  --molecules 2 --threshold 2 \
+  --output-plot kv_1000g.pdf --plot-x-bin 1 \
+  --output-counts kv_1000g.tab \
+  --cores 4
---
+++ examples/sg_100g.cmd
@@ -0,0 +1,5 @@
+trumicount --input-bam sg_100g.bam --umipair-sep '-'\
+  --paired --filter-strand-umis --molecules 1 --threshold 24 \
+  --output-plot sg_100g.pdf --plot-x-bin 3 \
+  --output-counts sg_100g.tab \
+  --cores 4
