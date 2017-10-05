0addeab6640cd936a53667880a460bbc0d3cb29b
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-10-05 18:03:56 +0200

    Removed --cores 4 from sg_100g.cmd and sh_100g_comb.cmd

--- examples/sg_100g_comb.cmd
+++ examples/sg_100g_comb.cmd
@@ -1,5 +1,4 @@
 trumicount --input-bam sg_100g.bam --umipair-sep '-'\
   --paired --combine-strand-umis --molecules 1 --threshold 24 \
   --output-plot sg_100g_comb.pdf --plot-x-bin 3 \
-  --output-counts sg_100g_comb.tab \
-  --cores 4
+  --output-counts sg_100g_comb.tab
