3cd79d431530bba6fe030483cf12d491f11ad934
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-10-09 00:43:44 +0200

    Renamed plot-related command-line arguments, factored PDF open/close out
    This is in preparation of adding a second plot to the output PDF

--- examples/kv_1000g.cmd
+++ examples/kv_1000g.cmd
@@ -1,5 +1,5 @@
 trumicount --input-bam kv_1000g.bam \
   --molecules 2 --threshold 2 \
-  --output-plot kv_1000g.pdf --plot-x-bin 1 \
+  --output-plots kv_1000g.pdf --plot-hist-bin 1 \
   --output-counts kv_1000g.tab \
   --cores 4
--- examples/sg_100g.cmd
+++ examples/sg_100g.cmd
@@ -1,4 +1,4 @@
 trumicount --input-bam sg_100g.bam --umipair-sep '-'\
   --paired --filter-strand-umis --molecules 1 --threshold 24 \
-  --output-plot sg_100g.pdf --plot-x-bin 3 \
+  --output-plots sg_100g.pdf --plot-hist-bin 3 \
   --output-counts sg_100g.tab
