26f2fc6aea90e20ad1dd26b088c2e362bea81092
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-10-10 00:52:04 +0200

    Showcase --plot-var-logy and --plot-var-bins in examples (kv_1000g.cmd)

--- examples/kv_1000g.cmd
+++ examples/kv_1000g.cmd
@@ -1,5 +1,6 @@
 trumicount --input-bam kv_1000g.bam \
   --molecules 2 --threshold 2 \
   --output-plots kv_1000g.pdf --plot-hist-bin 1 \
+  --plot-var-bins 20 --plot-var-logy \
   --output-counts kv_1000g.tab \
   --cores 4
