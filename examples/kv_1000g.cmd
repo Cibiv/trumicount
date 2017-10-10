56798a27c5165f271abe28eb4f70337bb4081a36
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-10-10 15:38:21 +0200

    Updated example results

--- examples/kv_1000g.cmd
+++ examples/kv_1000g.cmd
@@ -1,5 +1,5 @@
 trumicount --input-bam kv_1000g.bam \
-  --molecules 2 --threshold 2 \
+  --molecules 2 --threshold 2  --genewise-min-umis 3 \
   --output-plots kv_1000g.pdf --plot-hist-bin 1 \
   --plot-var-bins 20 --plot-var-logy \
   --output-counts kv_1000g.tab \
