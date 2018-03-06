3cc453f3332f711e72db594a36ff7d16514176ab
Florian G. Pflug <florian.pflug@univie.ac.at>
2018-03-06 20:03:09 +0100

    Added --digits option to trumicount, and changes the tests to output only 2 sig. digits
    This will hopefully make the results the same on OSX and linux, allowing
    us to use a single set of expected file for both.

--- tests/testcases/sg_100g.cmd
+++ tests/testcases/sg_100g.cmd
@@ -1,4 +1,4 @@
 $TRUMICOUNT --input-umitools-group-out "$TESTCASES/sg_100g.gout.bz2" \
 	--paired --filter-strand-umis --umipair-sep '-' \
 	--molecules 1 --threshold 24 \
-	--output-counts sg_100g.out
+	--output-counts sg_100g.out --digits 2
