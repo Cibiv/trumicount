40e911e6dba7f6466523e063751fd4488f1a15d7
Florian G. Pflug <florian.pflug@univie.ac.at>
2018-07-06 19:24:54 +0200

    Make it possible to use an already existing WORKDIR for tests/run_tests_in_testenv.sh

--- tests/run_tests_in_testenv.sh
+++ tests/run_tests_in_testenv.sh
@@ -24,28 +24,37 @@ export TESTS="$( cd "$(dirname "$0")" ; pwd -P )"
 source "$TESTS/dependencies.sh"
 
 # Create temporary working directory, make it the cwd
-WORKDIR="$(mktemp -d)"
-if ! test -d "$WORKDIR"; then
-	WORKDIR=""
-	echo "Failed to create temporary working directory ($WORKDIR is not a directory):" >&2
-	exit 1
-fi
-pushd "$WORKDIR" >/dev/null
-echo "=== Created working directory $WORKDIR"
-
-# Cleanup on exit
-function on_exit() {
-	popd >/dev/null
-	if test "$WORKDIR" != "" && test "$KEEP_WORKDIR" == ""; then
-		echo "=== Removing working directory $WORKDIR"
-		rm -rf "$WORKDIR"
+if [[ "$WORKDIR" = "" ]]; then
+	WORKDIR="$(mktemp -d)"
+	if ! test -d "$WORKDIR"; then
+		WORKDIR=""
+		echo "Failed to create temporary working directory ($WORKDIR is not a directory):" >&2
+		exit 1
 	fi
-}
-trap on_exit EXIT
+	pushd "$WORKDIR" >/dev/null
+	# Cleanup on exit
+	function on_exit() {
+		popd >/dev/null
+		if test "$WORKDIR" != "" && test "$KEEP_WORKDIR" == ""; then
+			echo "=== Removing working directory $WORKDIR"
+			rm -rf "$WORKDIR"
+		fi
+	}
+	trap on_exit EXIT
+	echo "=== Created working directory $WORKDIR"
+else
+	pushd "$WORKDIR" >/dev/null
+	echo "=== Using working directory $WORKDIR"
+fi
 
 # Setup conda test environment
-echo "=== Creating and activating test environment in $WORKDIR/testenv"
-conda create --no-default-packages --use-index-cache --yes -p "$WORKDIR/testenv" --file "$TESTS/$TESTENV_PKGS"
+if [[ ! -d "$WORKDIR/testenv" ]]; then
+	echo "=== Creating and activating test environment in $WORKDIR/testenv"
+	conda create --no-default-packages --use-index-cache --yes -p "$WORKDIR/testenv" --file "$TESTS/$TESTENV_PKGS"
+else
+	echo "=== Activating ALREADY EXISTING test environment in $WORKDIR/testenv"
+	echo "=== Existing environment is assumed to be consistent with $TESTENV_PKGS!"
+fi
 source activate "$WORKDIR/testenv"
 
 # Ensure that R uses only packages from the test environment
