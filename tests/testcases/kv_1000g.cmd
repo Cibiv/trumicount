1b643ca7531d0facca9c6b025982e4d2284af437
Florian G. Pflug <florian.pflug@univie.ac.at>
2019-01-25 16:08:01 +0100

    Modified the kv_1000g testcase to rely on --threshold-quantile

--- tests/testcases/kv_1000g.cmd
+++ tests/testcases/kv_1000g.cmd
@@ -1,3 +1,3 @@
 $TRUMICOUNT --input-umitools-group-out "$TESTCASES/kv_1000g.gout.bz2" \
-	--molecules 2 --threshold 2  --genewise-min-umis 3 \
+	--molecules 2 --genewise-min-umis 3 \
 	--output-counts kv_1000g.output --digits 2
